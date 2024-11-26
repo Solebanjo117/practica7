b9f7270e349e7fb34c362a361108f064dc144dc9
JUAN ANTONIO CASTILLA ORTA <L21430327@piedrasnegras.tecnm.mx>
2024-11-26 12:45:28 -0600

    nose

--- resources/views/horarioAlumn.blade.php
+++ resources/views/horarioAlumn.blade.php
@@ -1,14 +1,7 @@
 @extends('dashboard')
 @section('contenido')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alta de Horario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+
+
     <div class="container mt-5">
         <h1 class="text-center mb-4">Alta de Horario</h1>
 
@@ -77,8 +70,8 @@
         </form>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+  
+
+
 
 @endsection
