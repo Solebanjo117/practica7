778d8542b99f0aaf5423bd4ce5f6824a2adc46c2
JUAN ANTONIO CASTILLA ORTA <L21430327@piedrasnegras.tecnm.mx>
2024-12-12 22:23:33 -0600

    cors2

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,13 +8,7 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-
-        // Agregar las cabeceras de CORS
-        $response->headers->set('Access-Control-Allow-Origin', '*'); // Cambia '*' por el dominio permitido, por ejemplo: 'http://localhost:3000'
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-
-        return $response;
+        return $next($request)->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+            ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
     }
 }
