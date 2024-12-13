781590ae5eb368bd237cbdd05cc239f2690c3d41
JUAN ANTONIO CASTILLA ORTA <L21430327@piedrasnegras.tecnm.mx>
2024-12-12 23:03:39 -0600

    pruebas

--- routes/api.php
+++ routes/api.php
@@ -15,18 +15,18 @@ Route::get('/archivos/{noctrl}',[JsonController::class,'archivo']);
 Route::get('/gruposHorarios/{id}',[JsonController::class,'gruposHorarios']);
 Route::get('/materias-disponibles/{turno}/{alumno_id}', [JsonController::class, 'obtenerMaterias']);
 Route::post('/inscribir-alumno', [JsonController::class, 'inscribirAlumno']);
-Route::get('horario-alumno/{alumnoId}', [JsonController::class, 'getHorarioAlumno']);
-Route::post('insOrUpdtGrpo',[JsonController::class,'insOrUpdtGrpo']);
-Route::get('departamentos',[JsonController::class,'departamentos']);
-Route::get('edificios',[JsonController::class,'edificios']);
-Route::get('lugares',[JsonController::class,'lugares']);
-Route::get('personal',[JsonController::class,'personal']);
-Route::get('materias',[JsonController::class,'materias']);
-Route::post('insertarHorario',[JsonController::class,'insertHorario']);
-Route::post('borrarHorario',[JsonController::class,'borrarHorario']);
-Route::get('horarios',[JsonController::class,'horarios']);
-Route::post('agregarMateria',[JsonController::class,'aggMat']);
-Route::post('agregarProfe',[JsonController::class,'aggProf']);
+Route::get('/horario-alumno/{alumnoId}', [JsonController::class, 'getHorarioAlumno']);
+Route::post('/insOrUpdtGrpo',[JsonController::class,'insOrUpdtGrpo']);
+Route::get('/departamentos',[JsonController::class,'departamentos']);
+Route::get('/edificios',[JsonController::class,'edificios']);
+Route::get('/lugares',[JsonController::class,'lugares']);
+Route::get('/personal',[JsonController::class,'personal']);
+Route::get('/materias',[JsonController::class,'materias']);
+Route::post('/insertarHorario',[JsonController::class,'insertHorario']);
+Route::post('/borrarHorario',[JsonController::class,'borrarHorario']);
+Route::get('/horarios',[JsonController::class,'horarios']);
+Route::post('/agregarMateria',[JsonController::class,'aggMat']);
+Route::post('/agregarProfe',[JsonController::class,'aggProf']);
 
 
 
